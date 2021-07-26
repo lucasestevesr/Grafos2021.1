70bf40535fb4fa839c7d177f9d18ba7c4d59c421
Pedro Paulo <pedropaulosilvaf@gmail.com>
2021-07-26 19:04:58 -0300

    organizando código pt.1

---
+++ cmake-build-debug/CMakeFiles/Grafos2021_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Grafos2021_1.dir/Edge.cpp.obj"
+  "CMakeFiles/Grafos2021_1.dir/Graph.cpp.obj"
+  "CMakeFiles/Grafos2021_1.dir/Node.cpp.obj"
+  "CMakeFiles/Grafos2021_1.dir/main.cpp.obj"
+  "Grafos2021_1.exe"
+  "Grafos2021_1.exe.manifest"
+  "Grafos2021_1.pdb"
+  "libGrafos2021_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Grafos2021_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
